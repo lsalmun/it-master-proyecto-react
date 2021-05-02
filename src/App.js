1a8fe706787589fb02ccdffa4ff66134f8491371
lsalmun <lsalmun@hotmail.com>
2021-05-01 23:41:20 -0300

    subida proy it master react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+
+
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+    <div id="first-nav">
+        <a href="#footer">Sign in</a> or
+        <a href="#footer">create an account</a>
     </div>
-  );
-}
+    <div id="second-nav">
+        <img className="logo" src="img/logo.png" alt="logo Google" />
 
-export default App;
+        <div className="second-nav-menu">
+            <div className="second-nav-hamburger">
+                <img src="img/hamburguesa.svg" alt=""/>
+            </div>
+            
+            <div className="second-nav-links">
+            </div>
+    </div>
+    </div>
+
+    <div id="banner">
+        <h1>Lorem Ipsum</h1>
+    <button>Lern more</button>
+    </div>
+
+    <div id="items">
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div> 
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+    
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+    
+        <div className="item-product">
+         <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+    
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+
+        <div className="item-product">
+        <img src="https://placeimg.com/400/400/arch" alt="logo Google" />
+        <p><b>Product category one</b> product category one product category one product category one product category one</p>
+        </div>
+        </div>
+
+    <div id="cookie">
+        este sitio utiliza cookies
+        <a href="#">Aceptar</a>
+    </div>   
+
+
+
+    <div id="footer">
+        <p>Suscribite al newsletter</p>
+        <form action="" method="post">
+            <input type="text" />
+            <button>Enviar</button>
+        </form>
+    </div>
+
+    </div>
+  )
+}
+export default App
