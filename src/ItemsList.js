6544068ad967fdff3cf2ea515272e1f8a6e8d028
lsalmun <lsalmun@hotmail.com>
2021-05-24 01:26:15 -0300

    paginador

--- src/ItemsList.js
+++ src/ItemsList.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import items from './items.js'
 
 
@@ -5,17 +6,50 @@ import items from './items.js'
 
 
 function ItemsList () {
-    return (<div className="items">
+
+    let [page, setPage] = useState(4)
+    let a = 0
+    
+    let pages = items.slice(a,page)
+    
+    
+    function nextPage() {
+       
+
+        setPage(page + 4)
+        
+    }
+    return (
+    <>
+    <div className="items">
         {
-            items.map(function(dato){ 
-            return ( <div className="item-product" key={dato.id}>
+            pages.map(function(dato){ 
+            return (
+                
+                <div className="item-product" key={dato.id}>
      
                      <img src= {dato.image} />
                      {dato.title}
+                     {dato.id}
                      
                     </div>)
              })}
         </div>     
+
+        <button onClick={() => setPage(page=4)}>
+                Primera pagina
+            </button>
+
+       <button className="App-ItemsList-paginado" onClick={nextPage}>
+            VER MAS PRODUCTOS
+
+        </button>
+
+        <button onClick={() => setPage(page=items.length)}>
+            Ultima pagina
+        </button>
+
+        </>
       )}
 
 
